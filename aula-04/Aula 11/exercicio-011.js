ad7dd50ea484225580f12eded38e244bb2c83041
luizfernando637 <99849840+luizfernando637@users.noreply.github.com>
2023-06-28 10:37:38 -0300

    atualização

--- aula-04/Aula 11/exercicio-011.js
+++ aula-04/Aula 11/exercicio-011.js
@@ -1,2 +1,19 @@
-var hora = 8 
+var agora = new Date()
+var hora = agora.getHours() 
 console.log(`Agora exatamente são ${hora} horas`)
+
+if(hora < 12) {
+
+    console.log('Bom dia')
+}
+
+else if(hora <= 18) {
+
+    console.log('Boa Tarde')
+
+}
+
+else {
+
+    console.log('Boa Noite') 
+}
