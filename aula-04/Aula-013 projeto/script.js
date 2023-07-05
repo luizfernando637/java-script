0ef48f79006ca623cf340ab6965f3d9295daac8e
luizfernando637 <99849840+luizfernando637@users.noreply.github.com>
2023-07-05 12:58:05 -0300

    atualização

--- aula-04/Aula-013 projeto/script.js
+++ aula-04/Aula-013 projeto/script.js
@@ -7,19 +7,19 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
         // bom dia 
-        img.src = `imagens/imagem dia.png`
+        img.src = ('imagens/imagem dia.png')
         document.body.style.background = '#FAF164'
     }
 
     else if (hora >= 12 && hora < 18) {
         // boa tarde
-        img.src = 'imagem da tarde.png'
+        img.src = ('imagens/imagem da tarde.png')
         document.body.style.background = '#98DE59'
     }
 
     else {
         //boa noite 
-        img.src = 'imagem noite.png'
+        img.src = ('imagens/imagem noite.png')
         document.body.style.background = '#000000'
     }
 }
