b5fc5be3408ab496ddf91c30f9e1154eea59e8ec
luizfernando637 <99849840+luizfernando637@users.noreply.github.com>
2023-07-10 10:22:35 -0300

    atualização do projeto

---
+++ Aula-014 projeto/script.js
@@ -0,0 +1,25 @@
+function carregar() {
+
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        // bom dia 
+        img.src = ('imagens/imagem dia.png')
+        document.body.style.background = '#FAF164'
+    }
+
+    else if (hora >= 12 && hora < 18) {
+        // boa tarde
+        img.src = ('imagens/imagem da tarde.png')
+        document.body.style.background = '#98DE59'
+    }
+
+    else {
+        //boa noite 
+        img.src = ('imagens/imagem noite.png')
+        document.body.style.background = '#000000'
+    }
+}
